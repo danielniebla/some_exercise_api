41cc38ab3750e1ce248917c2eb599b78c0c41a25
danieniebla <82241926+danielniebla@users.noreply.github.com>
2024-07-18 14:27:09 -0700

    ax5

--- exerciseCRUD/vite.config.js
+++ exerciseCRUD/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist', // Asegúrate de que Vercel esté buscando en este directorio
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
